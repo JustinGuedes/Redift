b039b301adcea9280d276af68aa32fc4ca0e7cd2
Justin Guedes <justin.guedes@gmail.com>
2018-08-01 16:58:51 +0200

    Update package version

--- Package.swift
+++ Package.swift
@@ -6,7 +6,6 @@ import PackageDescription
 let package = Package(
     name: "Redift",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "Redift",
             targets: ["Redift"]),
@@ -15,18 +14,16 @@ let package = Package(
             targets: ["Redift"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        //.package(url: "https://github.com/JustinGuedes/Prelude", from: "0.0.0")
-    	.package(url: "../Prelude", .branch("master"))
+        .package(url: "https://github.com/JustinGuedes/Prelude", from: "0.0.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Redift",
-            dependencies: ["Prelude"]),
+            dependencies: ["Prelude"],
+            path: "Sources"),
         .testTarget(
             name: "RediftTests",
-            dependencies: ["Redift"]),
+            dependencies: ["Redift"],
+            path: "Tests")
     ]
 )
